1d1e219287c2b7ca5e8a1387fe49ecf58fa94c7c
Fresemin <59062254+FreSemin@users.noreply.github.com>
2020-01-26 19:55:31 +0300

    Add files via upload

---
+++ Alexis/js/script.js
@@ -0,0 +1,27 @@
+window.onload = function() {
+  let preloader = document.getElementById("loader");
+  preloader.style.display = "none";
+};
+
+window.onscroll = function() {
+  let scrolled = window.pageYOffset || document.documentElement.scrollTop;
+  let notFixedMenu = document.getElementById("fixed");
+
+  if (scrolled > 100) {
+    notFixedMenu.classList.add("fixed");
+  } else {
+    notFixedMenu.classList.remove("fixed");
+  }
+};
+
+function classToggle() {
+  {
+    this.classList.toggle("menu_toggle");
+  }
+  {
+    this.classList.toggle("menu_active_2");
+    document.querySelector(".main_menu").classList.toggle("menu_active");
+  }
+}
+
+document.querySelector(".menu_toggle").addEventListener("click", classToggle);
