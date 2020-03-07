8012934ec050882c37060f37f3d207932ca0513f
Fresemin <andrew45593@gmail.com>
2020-03-07 14:02:33 +0300

    KENIGZ(fix sub nenu, add file btn)
    +- normal

---
+++ KENIGZ/js/script.js
@@ -0,0 +1,16 @@
+    let inputs = document.querySelectorAll('#input__file');
+    Array.prototype.forEach.call(inputs, function (input) {
+      let label = input.nextElementSibling,
+        labelVal = label.querySelector('.label_file_name').innerText;
+ 
+      input.addEventListener('change', function (e) {
+        let countFiles = '';
+        if (this.files && this.files.length >= 1)
+          countFiles = (this.files);
+ 
+        if (countFiles)
+          document.querySelector('.file_name').innerText = "Файл выбран";
+        else
+        document.querySelector('.file_name').innerText = "Файл не выбран";
+      });
+    });
