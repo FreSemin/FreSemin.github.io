8b2d628bb591a40ce17f5dd19ccb610e86d62bc9
FreSemin <andrew45593@gmail.com>
2023-11-25 13:48:42 +0300

    fix(menu): close on mobile

--- Stimulus/js/main.js
+++ Stimulus/js/main.js
@@ -1,4 +1,20 @@
 "use strict";
 
-// Start WOW library
-new WOW().init();
+let menuBtn, menuLinks;
+
+function toggleMenu() {
+  if (menuBtn.checked) {
+    menuBtn.checked = false;
+  }
+}
+
+document.addEventListener("DOMContentLoaded", function () {
+  // Start WOW library
+  new WOW().init();
+
+  menuBtn = document.getElementById("menu-check");
+  menuLinks = document.querySelectorAll(".menu-list__item-link");
+  menuLinks.forEach((menuLink) => {
+    menuLink.addEventListener("click", toggleMenu);
+  });
+});
